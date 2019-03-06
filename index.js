00f1cf1e8578ebc19c2fb76dde544ef52193e6fe
statox <me@statox.fr>
2019-03-06 18:28:58 +0100

    Clean up some code

--- index.js
+++ index.js
@@ -6,6 +6,54 @@ const https = require('https');
 
 const dir = '/tmp/';
 
+const setWallpaper = (imagePath) => {
+    console.log("setting wallpaper");
+    const options = {
+        scale: 'center'
+    }
+
+    const desktopCommand = `gsettings set org.gnome.desktop.background picture-uri \'file:///${imagePath}\'`;
+    const desktopSettingsCommand = `gsettings set org.gnome.desktop.background picture-options \'stretched\'`;
+    const screensaverCommand = `gsettings set org.gnome.desktop.screensaver picture-uri \'file:///${imagePath}\'`;
+    const screensaverSettingsCommand = `gsettings set org.gnome.desktop.screensaver picture-options \'stretched\'`;
+
+    exec(desktopCommand, (error, _stdout, _stderr) => {
+        if (error) {
+            console.log("error while setting desktop wallpaper");
+            console.log(error);
+            return;
+        }
+        console.log("Desktop wallpaper set successfully");
+    });
+
+    exec(desktopSettingsCommand, (error, _stdout, _stderr) => {
+        if (error) {
+            console.log("error while setting desktop wallpaper options");
+            console.log(error);
+            return;
+        }
+        console.log("Desktop wallpaper options set successfully");
+    });
+
+    exec(screensaverCommand, (error, _stdout, _stderr) => {
+        if (error) {
+            console.log("error while setting screensaver wallpaper");
+            console.log(error);
+            return;
+        }
+        console.log("Screensaver wallpaper set successfully");
+    });
+
+    exec(screensaverSettingsCommand, (error, _stdout, _stderr) => {
+        if (error) {
+            console.log("error while setting desktop wallpaper options");
+            console.log(error);
+            return;
+        }
+        console.log("Desktop wallpaper options set successfully");
+    });
+};
+
 const linkSplitter = data => {
     return data.split('<a href="image')[1].split('"')[0];
 };
@@ -19,56 +67,12 @@ const downloadImage = (imageSource, picture) => {
         console.log(`Picture saved at ${dir}${picture}`);
 
         save.on('finish', () => {
+            setWallpaper(`${dir}${picture}`);
             save.close(cb);
-            console.log("setting wallpaper");
-            const options = {
-                scale: 'center'
-            }
+        });
 
-            const desktopCommand = `gsettings set org.gnome.desktop.background picture-uri \'file:///${dir}${picture}\'`;
-            const desktopSettingsCommand = `gsettings set org.gnome.desktop.background picture-options \'stretched\'`;
-            const screensaverCommand = `gsettings set org.gnome.desktop.screensaver picture-uri \'file:///${dir}${picture}\'`;
-            const screensaverSettingsCommand = `gsettings set org.gnome.desktop.screensaver picture-options \'stretched\'`;
-
-            exec(desktopCommand, (error, _stdout, _stderr) => {
-                if (error) {
-                    console.log("error while setting desktop wallpaper");
-                    console.log(error);
-                    return;
-                }
-                console.log("Desktop wallpaper set successfully");
-            });
-
-            exec(desktopSettingsCommand, (error, _stdout, _stderr) => {
-                if (error) {
-                    console.log("error while setting desktop wallpaper options");
-                    console.log(error);
-                    return;
-                }
-                console.log("Desktop wallpaper options set successfully");
-            });
-
-            exec(screensaverCommand, (error, _stdout, _stderr) => {
-                if (error) {
-                    console.log("error while setting screensaver wallpaper");
-                    console.log(error);
-                    return;
-                }
-                console.log("Screensaver wallpaper set successfully");
-            });
-
-            exec(screensaverSettingsCommand, (error, _stdout, _stderr) => {
-                if (error) {
-                    console.log("error while setting desktop wallpaper options");
-                    console.log(error);
-                    return;
-                }
-                console.log("Desktop wallpaper options set successfully");
-            });
-
-            save.on('error', () => {
-                process.exit(1);
-            });
+        save.on('error', () => {
+            process.exit(1);
         });
     });
 };
