30b471d9f6400f1eb43fe60150353a58a803d972
statox <me@statox.fr>
2019-03-07 12:06:31 +0100

    Remove previous wallpapers

--- index.js
+++ index.js
@@ -1,56 +1,80 @@
+const async = require('async');
+
 const exec = require('child_process').exec;
 const got = require('got');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const https = require('https');
 
-const dir = '/tmp/';
+const dir = '/home/afabre/.wallpaper/';
+
+const removeOldPictures = () => {
+    const rmCommand = `find ${dir} -mtime +3 -delete`
+
+    exec(rmCommand, (error, _stdout, _stderr) => {
+        if (error) {
+            console.log("error while deleting previous wallpapers");
+            console.log(error);
+            return;
+        }
+        console.log("Previous wallpapers deleted successfully");
+    });
+};
 
 const setWallpaper = (imagePath) => {
     console.log("setting wallpaper");
-    const options = {
-        scale: 'center'
-    }
 
     const desktopCommand = `gsettings set org.gnome.desktop.background picture-uri \'file:///${imagePath}\'`;
     const desktopSettingsCommand = `gsettings set org.gnome.desktop.background picture-options \'stretched\'`;
     const screensaverCommand = `gsettings set org.gnome.desktop.screensaver picture-uri \'file:///${imagePath}\'`;
     const screensaverSettingsCommand = `gsettings set org.gnome.desktop.screensaver picture-options \'stretched\'`;
 
-    exec(desktopCommand, (error, _stdout, _stderr) => {
+    async.auto({
+        desktopWallpaper: (cb) => exec(desktopCommand, (error, _stdout, _stderr) => {
+            if (error) {
+                console.log("error while setting desktop wallpaper");
+                console.log(error);
+                return cb(error);
+            }
+            console.log("Desktop wallpaper set successfully");
+            return cb();
+        }),
+        desktopSetting: (cb) => exec(desktopSettingsCommand, (error, _stdout, _stderr) => {
+            if (error) {
+                console.log("error while setting desktop wallpaper options");
+                console.log(error);
+                return cb(error);
+            }
+            console.log("Desktop wallpaper options set successfully");
+            return cb();
+        }),
+        screensaverWallpaper: (cb) => exec(screensaverCommand, (error, _stdout, _stderr) => {
+            if (error) {
+                console.log("error while setting screensaver wallpaper");
+                console.log(error);
+                return cb(error);
+            }
+            console.log("Screensaver wallpaper set successfully");
+            return cb();
+        }),
+        screensaverSettings: (cb) => exec(screensaverSettingsCommand, (error, _stdout, _stderr) => {
+            if (error) {
+                console.log("error while setting desktop wallpaper options");
+                console.log(error);
+                return cb(error);
+            }
+            console.log("Desktop wallpaper options set successfully");
+            return cb();
+        })
+    }, (error) => {
         if (error) {
-            console.log("error while setting desktop wallpaper");
+            console.log("We add an error in the async ececution");
             console.log(error);
             return;
         }
-        console.log("Desktop wallpaper set successfully");
-    });
 
-    exec(desktopSettingsCommand, (error, _stdout, _stderr) => {
-        if (error) {
-            console.log("error while setting desktop wallpaper options");
-            console.log(error);
-            return;
-        }
-        console.log("Desktop wallpaper options set successfully");
-    });
-
-    exec(screensaverCommand, (error, _stdout, _stderr) => {
-        if (error) {
-            console.log("error while setting screensaver wallpaper");
-            console.log(error);
-            return;
-        }
-        console.log("Screensaver wallpaper set successfully");
-    });
-
-    exec(screensaverSettingsCommand, (error, _stdout, _stderr) => {
-        if (error) {
-            console.log("error while setting desktop wallpaper options");
-            console.log(error);
-            return;
-        }
-        console.log("Desktop wallpaper options set successfully");
+        console.log("Async execution went find, removing the previous wallpaper");
+        removeOldPictures();
     });
 };
 
@@ -60,6 +84,9 @@ const linkSplitter = data => {
 
 // Download image and set as wallpaper
 const downloadImage = (imageSource, picture) => {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
     const save = fs.createWriteStream(`${dir}${picture}`);
 
     https.get(imageSource, (res, cb) => {
