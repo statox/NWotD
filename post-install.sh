3b133b0537dcc728fd548d63def8ea825a6abee6
statox <me@statox.fr>
2019-04-04 10:42:37 +0200

    Add a post-install script to setup the app as auto start

---
+++ post-install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+LOG_FILE='/var/log/nwotd.log'
+AUTOSTART_DIR="$HOME/.config/autostart"
+AUTOSTART_FILE="$AUTOSTART_DIR/NWotD.desktop"
+
+# Add the application in Gnome's autostart directory
+mkdir -p "$AUTOSTART_DIR"
+touch "$AUTOSTART_FILE" 
+cat > "$AUTOSTART_FILE" << EOF
+[Desktop Entry]
+Type=Application
+Exec=/usr/bin/node $PWD/index.js >> $LOG_FILE 2>&1
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[en_US]=Nasa Wallpaper of the Day
+Name=Nasa Wallpaper of the Day
+Comment[en_US]=Set up Nasa Pic of the day as Wallpaper
+Comment=Set up Nasa Pic of the day as Wallpaper
+EOF
